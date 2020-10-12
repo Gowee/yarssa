fe41c54753a421507ead410c71025c21b8e37b01
Hung-I Wang <whygowe@gmail.com>
2020-10-12 14:12:54 +0800

    Bump build

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
   const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+  return response.status(200).send(`Hello ${name}!`)
 }
